e1e506d2c90f280d043fda059bc186924448d219
vvlgo <29571935@qq.com>
2018-06-14 16:38:22 +0800

    代理，自己找代理哈！！

--- proxyaby/aby.go
+++ proxyaby/aby.go
@@ -27,33 +27,3 @@ func (p AbuyunProxy) ProxyClient() http.Client {
 func init(){
 	Client=AbuyunProxy{AppID: proxyUser, AppSecret: proxyPass}.ProxyClient()
 }
-
-//func main()  {
-//	targetUrl := "http://test.abuyun.com"
-//	//targetUrl := "https://www.abuyun.com/switch-ip"
-//	//targetUrl := "https://www.abuyun.com/current-ip"
-//
-//	// 初始化 proxy http client
-//	client := AbuyunProxy{AppID: proxyUser, AppSecret: proxyPass}.ProxyClient()
-//
-//	request, _ := http.NewRequest("GET", targetUrl, bytes.NewBuffer([]byte(``)))
-//
-//	response, err := client.Do(request)
-//
-//	if err != nil {
-//		panic("failed to connect: " + err.Error())
-//	} else {
-//		bodyByte, err := ioutil.ReadAll(response.Body)
-//		if err != nil {
-//			fmt.Println("读取 Body 时出错", err)
-//			return
-//		}
-//		response.Body.Close()
-//
-//		body := string(bodyByte)
-//
-//		fmt.Println("Response Status:", response.Status)
-//		fmt.Println("Response Header:", response.Header)
-//		fmt.Println("Response Body:\n", body)
-//	}
-//}
